aa13e219c331580c959766d4bb3c930548f63b91
Marco Agresta <agrestagiovanni@yahoo.it>
2019-10-24 23:55:52 +0200

    Aggiunto mostro GS2 e corretto metodo scelta

--- src/utility/ScegliMostro.java
+++ src/utility/ScegliMostro.java
@@ -3,10 +3,11 @@ package utility;
 public class ScegliMostro {
 
 	LeggiFile file = new LeggiFile();
-	int lancio = Utility.lancio(1, 2);
+	int lancio = Utility.lancio(1, 3);
 	String mostro = "";
 	String apeGigante = "data\\GS1\\ape_gigante.txt";
 	String boletoStridente = "data\\GS1\\boleto_stridente.txt";
+	String arconteLanterna = "data\\GS2\\arconte_lanterna.txt";
 	
 	public String scelta() {
 		
@@ -14,8 +15,13 @@ public class ScegliMostro {
 		
 			case 1:
 				mostro = apeGigante;
+				break;
 			case 2:
 				mostro = boletoStridente;
+				break;
+			case 3:
+				mostro = arconteLanterna;
+				break;
 		}
 		return mostro;
 		 
